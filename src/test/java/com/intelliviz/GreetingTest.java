4aad005c68510a24292ab16e8f530478bde43577
emuhlestein <emuhlestein@yahoo.com>
2020-09-15 14:42:59 -0500

    Adding unit test

--- src/test/java/com/intelliviz/GreetingTest.java
+++ src/test/java/com/intelliviz/GreetingTest.java
@@ -1,19 +1,41 @@
 package com.intelliviz;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GreetingTest {
+    private Greeting greeting;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("In BeforeAll...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("In AfterAll...");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("In BeforeEach...");
+        greeting = new Greeting();
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In AfterEach...");
+        greeting = null;
+    }
+
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld());
     }
 
     @Test
     void helloWorld1() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld1("Ed"));
     }
 }
